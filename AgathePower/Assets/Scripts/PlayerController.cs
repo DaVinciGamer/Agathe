b273463cb957c516c9278f76eb32e45c5fe9db97
Jonas Scheid <64362990+j-scheid@users.noreply.github.com>
2024-06-23 17:41:56 +0200

    nochmal den Rest

--- AgathePower/Assets/Scripts/PlayerController.cs
+++ AgathePower/Assets/Scripts/PlayerController.cs
@@ -95,11 +95,17 @@ public class PlayerController : MonoBehaviour
         scoreText.text = "Score: " + score.ToString();
     }
 
-    public void updateHealth(int healthPoints){
-        currentHealth = currentHealth + healthPoints;
+    public void takeDamage(int healthPoints){
+        currentHealth -= healthPoints;
         healthBar.fillAmount = currentHealth / maxHealth;
         if(currentHealth == 0){
             //show Death screen
         }
+    }
+
+    public void heal(int healthPoints){
+        currentHealth += healthPoints;
+        currentHealth = Mathf.Clamp(healthPoints, 0, maxHealth);
+        healthBar.fillAmount = currentHealth / maxHealth;
     }
 }
